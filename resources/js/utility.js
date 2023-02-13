6d6d7d11df754a4067d13127acf09bf6425f0784
Anthony Talarico <astalarico@gmail.com>
2023-02-12 20:14:35 -0500

    adding pdf-lib dependency for dynamic pdf manipulation. adding basic async function for dealing with pdf form filling

--- resources/js/utility.js
+++ resources/js/utility.js
@@ -8,3 +8,39 @@ export const dataArrayToSelectOptions = data => {
         }
     );
 }
+
+export async function fillForm( formUrl ) {
+    // const formUrl = mid_database.plugin_url + "/public/assets/buyers-guide.pdf";
+    const formPdfBytes = await fetch(formUrl).then((res) => res.arrayBuffer());
+
+    const pdfDoc = await PDFDocument.load(formPdfBytes);
+    const form = pdfDoc.getForm();
+    const fields = form.getFields();
+    fields.forEach((field) => {
+        const type = field.constructor.name;
+        const name = field.getName();
+
+        if (type === "PDFTextField") {
+            field.setText("hi");
+        }
+        if (type === "PDFCheckBox") {
+            field.check();
+        }
+        if (type === "PDFRadioGroup") {
+            const options = field.getOptions();
+            console.log( options )
+            field.select(options[0]);
+ 
+        }
+        console.log(`${type}: ${name}`);
+
+
+    });
+    const pdfBytes = await pdfDoc.save();
+    var blob = new Blob([pdfBytes], { type: "application/pdf" });
+    var link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "myFileName.pdf";
+    link.click()
+    return window.URL.createObjectURL(blob);
+}
