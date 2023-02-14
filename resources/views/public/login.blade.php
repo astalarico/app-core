4c5135c54fa2a1d4f9c40d7d63729c069e7e35aa
Anthony Talarico <astalarico@gmail.com>
2023-02-13 21:26:47 -0500

    adjusting error messages on the login and register forms

--- resources/views/public/login.blade.php
+++ resources/views/public/login.blade.php
@@ -1,23 +1,26 @@
 <x-public-layout>
     <div class="h-full flex flex-col items-center justify-center">
-        @foreach ($errors->all() as $error)
-            <div class="bg-red-600 text-white p-2 mb-6">{{ $error }}</div>
-        @endforeach
-        <div id="form-container">
-            <img src="{{ getSettings()->app_logo }}" alt="" class="mb-14">
+        <div id="form-container" class="w-[320px]">
+            <img src="{{ getSettings()->app_logo }}" alt="" class="mb-14 max-w-[320px]">
             <form action="{{ route('login') }}" method="POST" class="">
                 @csrf
                 <div class="flex flex-col mb-4">
                     <label for="email">Email</label>
                     <input type="email" name="email" id="email" class="form-control rounded-md">
+                    @error('email')
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="flex flex-col mb-8">
                     <label for="password">Password</label>
                     <input type="password" name="password" id="password" class="!bg-white rounded-md">
+                    @error('password')
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
+
                 <div class="flex flex-col">
-                    <button type="submit"
-                        class="bg-[#CDEE2D] px-5 py-2 font-medium border border-b-4 border-r-4 border-black rounded-md">
+                    <button type="submit" class="bg-[#CDEE2D] px-5 py-2 font-medium border border-b-4 border-r-4 border-black rounded-md">
                         Login
                     </button>
                 </div>
--- resources/views/public/register.blade.php
+++ resources/views/public/register.blade.php
@@ -1,37 +1,30 @@
 <x-public-layout>
     <div class="h-full flex flex-col items-center justify-center">
 
-        <div id="form-container" class="relative">
-            {{-- <div id="errors-container" class="absolute top-1/2 right-full -translate-y-1/2 w-full">
-                @foreach ($errors->all() as $error)
-                    <div class="bg-red-600 text-white p-2 mb-2 rounded-md">{{ $error }}</div>
-                @endforeach
-            </div> --}}
-            <img src="{{ getSettings()->app_logo }}" alt="" class="mb-14">
+        <div id="form-container" class="w-[320px]">
+            <img src="{{ getSettings()->app_logo }}" alt="" class="mb-14 max-w-[320px]">
             <form action="{{ route('register') }}" method="POST" class="">
                 @csrf
                 <div class="flex flex-col mb-4">
                     <label for="email">Email</label>
                     <input type="email" name="email" id="email" class="form-control rounded-md">
                     @error('email')
-                        <div class="text-red-500">{{ $message }}</div>
+                    <div class="text-red-500">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="flex flex-col mb-8">
                     <label for="password">Password</label>
                     <input type="password" name="password" id="password" class="!bg-white rounded-md">
                     @error('password')
-                        <div class="text-red-500">{{ $message }}</div>
+                    <div class="text-red-500">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="flex flex-col mb-8">
                     <label for="password">Confirm Password</label>
-                    <input type="password" name="password_confirmation" id="password_confirmation"
-                        class="!bg-white rounded-md">
+                    <input type="password" name="password_confirmation" id="password_confirmation" class="!bg-white rounded-md">
                 </div>
                 <div class="flex flex-col">
-                    <button type="submit"
-                        class="bg-[#CDEE2D] px-5 py-2 font-medium border border-b-4 border-r-4 border-black rounded-md">
+                    <button type="submit" class="bg-[#CDEE2D] px-5 py-2 font-medium border border-b-4 border-r-4 border-black rounded-md">
                         Register
                     </button>
                 </div>
